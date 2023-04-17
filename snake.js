1b9f7a0e2bf6191573b418bc81a7328cf4af7db3
ifat2023 <ifat.elgart@gmail.com>
2023-04-17 21:39:01 +0300

    init

---
+++ snake.js
@@ -0,0 +1,13 @@
+let Square = document.getElementById("Square");
+
+
+let context = Square.getContext("2d")
+
+let window_height= window.innerHeight;
+let window_width = window.innerWidth;
+
+Square.width =  300; 
+Square.height = 300;
+
+
+console.log('hello')
